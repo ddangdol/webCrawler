a5bcb707c5314741904ad6669e68879318d3a425
yeongseok.jang <yeongseok.jang@yeongseokjangui-MacBook-Pro-208.local>
2018-09-23 17:00:32 +0900

    feat: 진입점부터 포스트 디테일까지 요청 플로우 작성 (closes #4)
    시작점인 http://newspeppermint.com/ 로 시작하여 파싱한 포스트 디테일
    url까지 호출 후 결과를 프린트한다.
    
    closes #4

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
-console.log('hello web crawler!!');
+const parsers = require('./parsers/parsers.js');
+const requests = require('./requests/requests.js');
+
+const limitPage = 3;
+const limitPosts = 12;
+
+function parsePageUrl(response) {
+  console.log('call parsePageUrl!!');
+  const paginationHtml = parsers.parsePagination(response.data);
+  const pageUrls = parsers.parsePageUrlsWithLimit(paginationHtml, limitPage);
+
+  pageUrls.forEach(function(pageUrl) {
+    requests.requestUrl(pageUrl, parsePostUrl);
+  });
+  parsePostUrl(response);
+}
+
+function parsePostUrl(response) {
+  console.log('call parsePostUrl!!');
+  const postUrls = parsers.parsePostUrlsWithLimit(response.data, limitPosts);
+
+  postUrls.forEach(function(postUrl) {
+    requests.requestUrl(postUrl, parsePost);
+  });
+}
+
+function parsePost(response) {
+  console.log('call parsePost!!');
+  let post = parsers.parsePost(response.data);
+  post.url = response.config.url;
+  console.log(post);
+}
+
+requests.requestUrl('http://newspeppermint.com/', parsePageUrl);
